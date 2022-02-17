8c9eb534504f4e8c2c90c21983b70836274f72fe
Davi Silva Santos <morg.daviss@gmail.com>
2022-02-17 16:01:12 -0300

    design od stroke

--- script2.js
+++ script2.js
@@ -3,4 +3,7 @@ let screen = document.getElementById("screen");
 let ctx = screen.getContext("2d");
 ctx.moveTo(0 , 0);
 ctx.lineTo(250, 250);
+ctx.lineTo(500, 0);
+ctx.lineWidth = 6;
+ctx.strokeStyle = "#ccff00";
 ctx.stroke();
