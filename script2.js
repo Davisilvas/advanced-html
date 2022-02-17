3fb263d03cf3b954faf4fbbee54deadc6c374eb3
Davi Silva Santos <morg.daviss@gmail.com>
2022-02-17 20:14:19 -0300

    more about canvas

--- script2.js
+++ script2.js
@@ -1,9 +1,22 @@
 let screen = document.getElementById("screen");
 
 let ctx = screen.getContext("2d");
-ctx.moveTo(0 , 0);
-ctx.lineTo(250, 250);
-ctx.lineTo(500, 0);
-ctx.lineWidth = 6;
-ctx.strokeStyle = "#ccff00";
+
+//CRIA UM RETÂNGULO PREENCHIDO E AZUL
+//ctx.fillStyle = "blue";
+//ctx.fillRect(10, 10, 100, 200);
+
+//CRIA UM RETÂNGULO VAZIO E VERMELHO
+//ctx.strokeStyle = "red";
+//ctx.strokeRect(10, 10, 100, 200);
+
+
+//CRIANDO UM RETÂNGULO COM BORDA VERMELHA E PREENCHIMENTO AZUL!
+ctx.rect(10, 10, 100, 200);
+ctx.strokeStyle = "red";
+ctx.lineWidth = 5;
+ctx.fillStyle = "blue";
+ctx.fill();
 ctx.stroke();
+
+ctx.clearRect(20, 20, 30, 30);
