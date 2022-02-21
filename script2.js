c44bd7cba60ff8f9a651571ca7846f368c9a6e38
Davi Silva Santos <morg.daviss@gmail.com>
2022-02-20 22:02:29 -0300

    images on canvas

--- script2.js
+++ script2.js
@@ -2,41 +2,15 @@ let screen = document.getElementById("screen");
 
 let ctx = screen.getContext("2d");
 
-let circle = {
-    x:  250,
-    y:  250,
-    raio: 100,
-    inicio: 0,
-    fim: 0 * Math.PI,
-    backwards: true,
+let img = new Image();
+img.src = "./assets/images/mordekai.png"
+
+img.onload = drawImg;
+
+/*function drawImg(){
+    ctx.drawImage(this, 20, 20);
+}*/
+
+function drawImg(){
+    ctx.drawImage(this, 20, 20, this.naturalWidth/2, this.naturalHeight /2);
 }
-
-function drawCircle(c){
-
-    ctx.beginPath();
-    ctx.rect(0, 0, 500, 500);
-    ctx.fillStyle = "beige";
-    ctx.fill();
-    
-    ctx.beginPath();
-    ctx.lineWidth = 5;
-    ctx.strokeStyle = "brown";
-    ctx.fillStyle = "blue";
-    ctx.arc(c.x, c.y, c.raio, c.inicio, c.fim, c.backwards);
-    ctx.fill();
-    ctx.stroke();
-}
-
-setInterval(function(){
-
-    if (circle.fim < 2 * Math.PI){
-        circle.fim += 0.3;
-        //circle.x -= 3;
-        circle.x += 3;
-
-    }
-
-    drawCircle(circle);
-
-
-}, 30)
